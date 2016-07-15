693c4b71000aa2a6a48246214dbfc6a028dd5849
StepSt <legionst@gmail.com>
2016-07-15 14:50:40 +0500

    Первый коммит

---
+++ WebServise_Pizza/Controllers/TelegramController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebServise_Pizza.Models;
+
+namespace WebServise_Pizza.Controllers
+{
+    public class TelegramController : ApiController
+    {
+        Telegram.Bot.Api bot = new Telegram.Bot.Api("256546691:AAHrj9UXnVMEWBbPLBxxfGFAKPN5HW5F9ys");
+        OrderContext db = new OrderContext();
+
+        // GET api/telegram/Update
+        [HttpPost]
+        #region
+        public void GetUpdate([FromBody]Update update)
+        {
+            switch (update.message.text)
+            {
+                case "/start":
+                    bot.SendTextMessageAsync(update.message.chat.id, "Cписок комманнд откроется по символу '/', в котором мы можете выбрать интересующий вас товар.");
+                    bot.SendTextMessageAsync(update.message.chat.id, "Привет," + update.message.from.first_name);
+                    break;
+                case "/getname":
+                    bot.SendTextMessageAsync(update.message.chat.id, "Список товаров, пришлите номер товара в списке");
+                    break;
+            }
+            int i;
+            if (int.TryParse(update.message.text, out i))
+            {
+                switch (update.message.text)
+                {
+                    case "1":
+                        db.Orders.Add(new Order { Name = "Товар11", Adress = "Адрес 11" });
+                        bot.SendTextMessageAsync(update.message.chat.id, "Заказ Товара 1 на Адрес 1 отправлен исполнителю");
+                        break;
+           //        case "1":
+            //        case "2":
+            //        case "3":
+            //            Book book = db.Books.Find(int.Parse(update.message.text));
+            //            bot.SendTextMessageAsync(update.message.chat.id, book.Name);
+            //            break;
+                    default:
+                        Order order = db.Orders.Find(int.Parse(update.message.text));
+                        bot.SendTextMessageAsync(update.message.chat.id, order.Name);
+                        break;
+                }
+
+            }
+        }
+        #endregion
+    }
+}
---
+++ WebServise_Pizza/Models/OrderingApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebServise_Pizza.Models
+{
+    public class OrderingApp : DropCreateDatabaseAlways<OrderContext>
+    {
+        protected override void Seed(OrderContext db)
+        {
+            db.Orders.Add(new Order { Name = "Товар1", Adress = "Адрес 1"});
+            db.Orders.Add(new Order { Name = "Товар2", Adress = "Адрес 2"});
+            db.Orders.Add(new Order { Name = "Товар3", Adress = "Адрес 3"});
+
+            base.Seed(db);
+        }
+    }
+}
